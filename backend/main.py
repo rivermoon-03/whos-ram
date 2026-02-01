77fad403eb7ca4ab35a47a053fa05393e8cb654b
rivermoon-03 <riverm00n@naver.com>
2026-02-01 18:47:06 +0900

    .

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
